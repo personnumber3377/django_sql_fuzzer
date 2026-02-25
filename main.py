9704345603acd5376f8fb50c1aa5539b62ec91c6
root <sontapaa.jokulainen@gmail.com>
2026-02-25 03:17:46 +0200

    Blah

--- main.py
+++ main.py
@@ -31,33 +31,6 @@ else:
         from util.errors import SAFE_EXCEPTIONS, IGNORED_MESSAGES
         seed_all()
 
-# def check_sql_query(sql_query_string):
-#     return True
-
-
-'''
-def check_sql_semantics(sql: str, payload: str):
-    # print("Checking!!!!!!!"*1000)
-    # 1. Raw dangerous tokens
-    for tok in [";", "--", "/*", "*/"]:
-        if tok in sql:
-            return False
-
-    # 2. Period-based alias confusion
-    if "." in payload:
-        # If payload used as alias,
-        # ensure it is quoted as single identifier
-        if payload in sql:
-            # good: full string preserved
-            return True
-        
-        # bad: split on period
-        parts = payload.split(".", 1)
-        if parts[0] in sql and parts[1] in sql:
-            return False
-    return True
-'''
-
 import re
 
 class SQLInjectionDetected(Exception):
@@ -151,15 +124,12 @@ def fuzz_entry(data: bytes):
     idx = data[0] % len(TARGETS)
     s = data[1:]
     s = sanitize_input(s)
-    # print("s: "+str(s))
     if s is None or len(s) < 2:
-        # print("Invalid input...")
         return
     payload = s # s[1:]
     try:
         dprint("calling "+str(TARGETS[idx])+" ...")
         sql_query = TARGETS[idx](payload)
-        # print("after...")
         if sql_query != None:
             dprint("sql_query: "+str(sql_query))
             # print(sql_query)
@@ -169,10 +139,6 @@ def fuzz_entry(data: bytes):
             else: 
                 check_sql_semantics(sql_query, payload)
             return
-            # Check the query thing...
-            # check_sql_semantics(sql_query, payload)
-            # if not check_sql_semantics(sql_query, payload):
-            #     raise SQLValidationError
     except SAFE_EXCEPTIONS as e:
         dprint(str(e))
         dprint("in safe exceptions...")
@@ -181,11 +147,6 @@ def fuzz_entry(data: bytes):
         # return
         msg = str(e)
         dprint(msg)
-        '''
-        print(msg)
-        print(IGNORED_MESSAGES)
-        '''
-
         if any(m in msg for m in IGNORED_MESSAGES):
             return
         # if "--" not in msg and msg.count(";") <= 1 and "/*" not in msg and "*/" not in msg:
@@ -217,19 +178,12 @@ def testing_main(): # Stuff...
         # rand_bytes = bytes([0x1a, 0x41, 0x19, 0xdb, 0x85])
         # 82 2a e7 87 b6
         # rand_bytes = bytes([0x82, 0x2a, 0xe7, 0x87, 0xb6])
-        
         # pwn = bytes([0x82, 0x2a, 0xe7, 0x87, 0xb6])
-
         # pwn = bytes([0x82, 0x01, 0x01]) # The index and then a 0x01 byte for the alias stuff. Should not work...
-
         # pwn = bytes([0x82, 0x2a, 0xe7, 0x87])
-
         # payload_thing = ";SELECT 1"
         # pwn += payload_thing.encode("ascii")
-
         # print(pwn)
-
-
         try:
             fuzz_entry(pwn)
             dprint("Done")
